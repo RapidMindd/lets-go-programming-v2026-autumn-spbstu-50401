96eae3123fb88af3702e8541b3d26199ad9cc659
RapidMindd <YaroslavT007@yandex.ru>
2026-09-20 19:35:29 +0300

    [TASK-1] calculator done

--- tarasenko.yaroslav/task-1/cmd/service/main.go
+++ tarasenko.yaroslav/task-1/cmd/service/main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var first_operand, second_operand int
+	var operator string
+	n, err := fmt.Scan(&first_operand, &second_operand, &operator)
+	if err != nil {
+		switch n {
+		case 0:
+			fmt.Println("Invalid first operand")
+		case 1:
+			fmt.Println("Invalid second operand")
+		case 2:
+			fmt.Println("Invalid operation")
+		}
+		return
+	}
+	switch operator {
+	case "+":
+		fmt.Println(first_operand + second_operand)
+	case "-":
+		fmt.Println(first_operand - second_operand)
+	case "*":
+		fmt.Println(first_operand * second_operand)
+	case "/":
+		if second_operand == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(first_operand / second_operand)
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
